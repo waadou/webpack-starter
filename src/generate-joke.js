47db77d21f3f28ea3db271e76066a5309606b056
ABODJI Alassani <abodjialassani@gmail.com>
2023-02-01 18:23:48 +0000

    Implementing jokes functionality using axios

--- src/generate-joke.js
+++ src/generate-joke.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+
+const config = {
+	headers: {
+		Accept: "application/json",
+	},
+};
+
+const jokesUrl = "https://icanhazdadjoke.com";
+
 const generateJoke = function () {
+	axios.get(jokesUrl, config).then((res) => {
+		document.getElementById("joke").innerHTML = res.data.joke;
+	});
 	return "I don't trust stairs. They are up to something.";
 };
 
--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ import logo from "./img/logo.svg";
 const logoImg = document.getElementById("logo");
 logoImg.src = logo;
 
-console.log(generateJoke());
+const jokeBtn = document.getElementById("jokeBtn");
+
+/** We want to get new joke when we clik on the joke button */
+jokeBtn.addEventListener("click", generateJoke);
+
+/** At least we want t have a joke when the page refreshes. */
+generateJoke();
